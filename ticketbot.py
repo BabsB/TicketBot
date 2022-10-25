bff1a834943e0210c26d3127fc271f87c55900ba
BabsB <babs_barnhoorn@hotmail.com>
2022-10-25 21:27:48 +0200

    Update ticketbot.py

--- ticketbot.py
+++ ticketbot.py
@@ -9,71 +9,65 @@ import selenium.webdriver
 import pickle
 import time
 
+def main():
+    browser = webdriver.Chrome(ChromeDriverManager().install())
+    # opens ticketswap
+    browser.get("https://www.ticketswap.nl/event/pip-opening-weekend/regular-tickets/bd69c0b0-3c0b-40c3-9e88-ac86ea76e9eb/2392838") #test
 
-browser = webdriver.Chrome(ChromeDriverManager().install())
-# opens ticketswap
-browser.get("https://www.ticketswap.nl/event/pip-opening-weekend/regular-tickets/bd69c0b0-3c0b-40c3-9e88-ac86ea76e9eb/2392838") #test
+    Tickets = False
 
+    #checks if logged in
+    checkLogIn = WebDriverWait(browser, 100).until(
+        EC.presence_of_element_located((By.CLASS_NAME,
+                                        "css-n4f2wf"))
+    )
 
+    while not Tickets:
+        try:
+            # refreshes the page looking for ticket
+            WebDriverWait(browser, 10).until_not(
+                EC.presence_of_element_located((By.CLASS_NAME,
+                                                "css-19fqo0n"))
+            )
+            print("it's not there")
+            time.sleep(2)
+            browser.refresh()
 
-Tickets = False
+        except:
+            # when ticket available, click on it
+            continueClick = WebDriverWait(browser, 5).until(
+                EC.presence_of_element_located((By.CLASS_NAME,
+                                                "css-19fqo0n"))
+            )
+            print('its there :)')
+            continueClick.click()
+            time.sleep(3)
 
-#checks if logged in
-checkLogIn = WebDriverWait(browser, 100).until(
-    EC.presence_of_element_located((By.CLASS_NAME,
-                                    "css-n4f2wf"))
-)
+            # add to cart
+            addCart = WebDriverWait(browser, 10).until(
+                EC.presence_of_element_located((By.CLASS_NAME,
+                                                "e1nefpxg2"))
+            )
 
-while not Tickets:
-    try:
-        # refreshes the page looking for ticket
+            addCart.click()
+            os.system('Ping.aiff')
+            os.system('Ping.aiff')
+            os.system('Ping.aiff')
 
-        WebDriverWait(browser, 10).until_not(
-            EC.presence_of_element_located((By.CLASS_NAME,
-                                            "css-19fqo0n"))
-        )
+            browser.refresh()
+            print('refreshed')
+            time.sleep(5)
 
-        print("it's not there")
-        time.sleep(2)
-        browser.refresh()
+            # go to cart
+            goToCart = WebDriverWait(browser, 20).until(
+                EC.presence_of_element_located((By.CLASS_NAME,
+                                                "e17kjwee0")))
 
-    except:
-        # when ticket available, click on it
-        continueClick = WebDriverWait(browser, 5).until(
-            EC.presence_of_element_located((By.CLASS_NAME,
-                                            "css-19fqo0n"))
-        )
-        print('its there :)')
-        continueClick.click()
-        time.sleep(3)
+            goToCart.click()
+            print('at cart')
+            time.sleep(1)
 
-        # add to cart
-        addCart = WebDriverWait(browser, 10).until(
-            EC.presence_of_element_located((By.CLASS_NAME,
-                                            "e1nefpxg2"))
-        )
+            Tickets = True
 
-        addCart.click()
-        os.system('Ping.aiff')
-        os.system('Ping.aiff')
-        os.system('Ping.aiff')
-
-        browser.refresh()
-        print('refreshed')
-        time.sleep(5)
-
-        # go to cart
-        goToCart = WebDriverWait(browser, 20).until(
-            EC.presence_of_element_located((By.CLASS_NAME,
-                                            "e17kjwee0")))
-
-        goToCart.click()
-        print('at cart')
-        time.sleep(1)
-
-        Tickets = True
-
-
-
-
+main()
 
